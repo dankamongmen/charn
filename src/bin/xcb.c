ac434187a1b410296e905956172956279e7abed6
nick black <nick.black@sprezzatech.com>
2013-03-31 01:41:36 -0400

    walk multiple screens

--- src/bin/xcb.c
+++ src/bin/xcb.c
@@ -11,12 +11,13 @@ int xcb_init(void){
 	xcb_randr_query_version_cookie_t rqvct;
 	xcb_randr_query_version_reply_t *rqvrt;
 	xcb_randr_screen_size_t *sizes,curgeom;
+	xcb_screen_iterator_t screenit;
 	int prefscr,cursize,numsizes;
 	const xcb_setup_t *xcbsetup;
 	xcb_generic_error_t *xcberr;
 	xcb_connection_t *xcb;
 	xcb_screen_t *xscr;
-	int z;
+	int z,scrcount;
 
 	if((xcb = xcb_connect(NULL,&prefscr)) == NULL){
 		fprintf(stderr,"Couldn't connect to $DISPLAY via XCB\n");
@@ -40,31 +41,37 @@ int xcb_init(void){
 		fprintf(stderr,"Couldn't get XRandR version info\n");
 		return EXIT_FAILURE;
 	}
-	printf("Connected using XRandR protocol %d.%d\n",
-			rqvrt->major_version,rqvrt->minor_version);
 	free(rqvrt);
-	sict = xcb_randr_get_screen_info(xcb,xscr->root);
-	if((sirt = xcb_randr_get_screen_info_reply(xcb,sict,&xcberr)) == NULL){
-		// FIXME use xcberr
-		fprintf(stderr,"Couldn't get XRandR screen info\n");
-		return EXIT_FAILURE;
-	}
-	cursize = sirt->sizeID;
-	numsizes = sirt->nSizes;
-	if((sizes = xcb_randr_get_screen_info_sizes(sirt)) == NULL){
-		fprintf(stderr,"Couldn't get XRandR size info\n");
-		return EXIT_FAILURE;
-	}
-	for(z = 0 ; z < numsizes ; ++z){
-		printf("[%02d] %4dx%-4d  ",z,sizes[z].width,sizes[z].height);
-		if(z % 4 == 3){
+	screenit = xcb_setup_roots_iterator(xcbsetup);
+	scrcount = screenit.rem;
+	printf("Connected using XRandR protocol %d.%d (%d screen%s)\n",
+			rqvrt->major_version,rqvrt->minor_version
+			,screenit.rem,screenit.rem == 1 ? "" : "s");
+	for(z = 0 ; z < scrcount ; ++z){
+		sict = xcb_randr_get_screen_info(xcb,screenit.data->root);
+		if((sirt = xcb_randr_get_screen_info_reply(xcb,sict,&xcberr)) == NULL){
+			// FIXME use xcberr
+			fprintf(stderr,"Couldn't get XRandR screen info\n");
+			return EXIT_FAILURE;
+		}
+		cursize = sirt->sizeID;
+		numsizes = sirt->nSizes;
+		if((sizes = xcb_randr_get_screen_info_sizes(sirt)) == NULL){
+			fprintf(stderr,"Couldn't get XRandR size info\n");
+			return EXIT_FAILURE;
+		}
+		for(z = 0 ; z < numsizes ; ++z){
+			printf("[%02d] %4dx%-4d  ",z,sizes[z].width,sizes[z].height);
+			if(z % 4 == 3){
+				printf("\n");
+			}
+		}
+		if(z % 4 != 2){
 			printf("\n");
 		}
+		memcpy(&curgeom,sizes + cursize,sizeof(curgeom));
+		free(sirt);
+		xcb_screen_next(&screenit);
 	}
-	if(z % 4 != 2){
-		printf("\n");
-	}
-	memcpy(&curgeom,sizes + cursize,sizeof(curgeom));
-	free(sirt);
 	return 0;
 }
