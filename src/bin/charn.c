4c7af5de32fc6654731a6c08e220be5d5b28b3ba
nick black <nick.black@sprezzatech.com>
2013-03-31 00:59:24 -0400

    get xdg reference

---
+++ src/bin/charn.c
@@ -0,0 +1,47 @@
+#include <errno.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <basedir.h>
+
+static void
+usage(const char *name,int status){
+	FILE *fp = status ? stderr : stdout;
+
+	fprintf(fp,"usage: %s [ options ]\n",name);
+	fprintf(fp,"options:\n");
+	fprintf(fp,"\t-h/--help: display this help text\n");
+	exit(status);
+}
+
+int main(int argc,char **argv){
+	const struct option longopts [] = {
+		{ "help",	0,	NULL,		'h'	},
+		{ NULL,		0,	NULL,		0	}
+	};
+	int opt,longopt;
+	xdgHandle xdg;
+
+	if((setlocale(LC_ALL,NULL)) == NULL){
+		fprintf(stderr,"Couldn't setlocale(LC_ALL)\n");
+		return EXIT_FAILURE;
+	}
+	while((opt = getopt_long(argc,argv,"h",longopts,&longopt)) >= 0){
+		switch(opt){
+			case 'h':
+				usage(argv[0],EXIT_SUCCESS);
+				break;
+			default:
+				usage(argv[0],EXIT_FAILURE);
+				break;
+		}
+	}
+	if(xdgInitHandle(&xdg) == NULL){
+		fprintf(stderr,"Couldn't initialize XDG data cache\n");
+		return EXIT_FAILURE;
+	}
+	printf("XDG [cache: %s] [config: %s]\n",xdgCacheHome(&xdg),xdgConfigHome(&xdg));
+	return EXIT_SUCCESS;
+}
