ec6a622e6f7e3d1f847c5306c37974f28e6f5fc5
Nick Black <nick.black@sprezzatech.com>
2013-04-25 21:49:47 -0400

    get and print GL implementation info

--- src/bin/glx.c
+++ src/bin/glx.c
@@ -2,6 +2,26 @@
 #include <stdio.h>
 #include <GL/glx.h>
 
+static int
+get_glx_vendor(void){
+	const GLubyte *vend,*rend,*ver;
+
+	if((vend = glGetString(GL_VENDOR)) == NULL){
+		fprintf(stderr,"Couldn't get OpenGL vendor\n");
+		return -1;
+	}
+	if((ver = glGetString(GL_VERSION)) == NULL){
+		fprintf(stderr,"Couldn't get OpenGL version\n");
+		return -1;
+	}
+	if((rend = glGetString(GL_RENDERER)) == NULL){
+		fprintf(stderr,"Couldn't get OpenGL renderer\n");
+		return -1;
+	}
+	printf("GL server: %s %s on %s\n",vend,ver,rend);
+	return 0;
+}
+
 int init_glx(Display *d,xcb_window_t window){
 	GLXFBConfig *glfb,ourfb;
 	int maj,min,numfbs;
@@ -36,6 +56,9 @@ int init_glx(Display *d,xcb_window_t window){
 	draw = glXCreateWindow(d,ourfb,window,0);
 	if(!glXMakeCurrent(d,draw,glctx)){
 		fprintf(stderr,"Couldn't activate GLX context\n");
+		return -1;
+	}
+	if(get_glx_vendor()){
 		return -1;
 	}
 	return 0;
