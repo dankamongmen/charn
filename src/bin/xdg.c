25f558e4b45412e9575aa660196e80b8e6cea599
Nick Black <nick.black@sprezzatech.com>
2013-04-30 10:48:42 -0400

    print XDG config directories

--- src/bin/xdg.c
+++ src/bin/xdg.c
@@ -5,11 +5,20 @@
 static xdgHandle xdg;
 
 int xdg_init(void){
+	const char * const *cdirs,* const *cdir;
+
 	if(xdgInitHandle(&xdg) == NULL){
 		fprintf(stderr,"Couldn't initialize XDG data cache\n");
 		return -1;
 	}
 	printf("XDG %.1f [cache: %s] [config: %s]\n",XDG_BASEDIR_SPEC,
 			xdgCacheHome(&xdg),xdgConfigHome(&xdg));
+	if((cdirs = xdgConfigDirectories(&xdg)) == NULL){
+		fprintf(stderr,"Couldn't get XDG config directories\n");
+		return -1;
+	}
+	for(cdir = cdirs ; *cdir ; ++cdir){
+		printf("XDG ConfigDirectory: %s\n",*cdir);
+	}
 	return 0;
 }
